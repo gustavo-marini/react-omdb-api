9970287be87dd8c0016bdc94425dd6416ff04c7c
Gustavo Marmentini <gustavomarini9@gmail.com>
2019-09-25 22:24:38 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const base_api_url = "http://www.omdbapi.com/";
+  const api_key = "d9ee1258";
+  const [movies, setMovies] = useState([]);
+
+  const handleFormSubmit = (e) => {
+    if (e.key === 'Enter') {
+      var movie_name = e.target.value;
+
+      var url = base_api_url + "?s=" + encodeURI(movie_name) + "&apikey=" + api_key;
+
+      const exec = async() => {
+        const resposta = await fetch(url);
+        const json = await resposta.json();
+
+        var movies = json.Search;
+
+        setMovies(movies);
+      };
+      exec();
+    }
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      <div className="header">
+        <h1>OMDd API</h1>
+        <form id="search_movie" onSubmit={(e) => onSubmit(e)}>
+          <label>Nome do filme: </label>
+          <input name="movie_name" type="text" onKeyDown={(e) => handleFormSubmit(e)} />
+        </form>
+      </div>
+
+      <div className="list_movies">
+        <ul className="list">
+          {movies.map(movie => {
+            return (
+              <li className="movie">
+                <img src={movie.Poster} />
+                <div className="movie_info">
+                  <span id="title"><strong>Título: </strong>{movie.Title}</span>
+                  <span id="year"><strong>Ano de lançamento: </strong>{movie.Year}</span>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
