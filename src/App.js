fc57d30227004713cca0e57e7152c2901fad22db
Secco2112 <gustavomarini9@gmail.com>
2019-10-09 01:36:29 -0300

    Comparar filmes

--- src/App.js
+++ src/App.js
@@ -1,98 +1,135 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faThumbsDown, faThumbsUp, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const base_api_url = "http://www.omdbapi.com/";
-  const api_key = "d9ee1258";
-  const [movies, setMovies] = useState([]);
-  const [favorites, setFavorites] = useState([]);
+	const base_api_url = "http://www.omdbapi.com/";
+	const api_key = "d9ee1258";
+	const [movies, setMovies] = useState([]);
+    const [favorites, setFavorites] = useState([]);
+    const [compareMovies, setCompareMovies] = useState([]);
 
-  useEffect(() => {
-    var tmp = localStorage.getItem("favorites");
-    if(tmp) {
-      var fav = tmp.split(",");
-      setFavorites(fav);
-    } else {
-      setFavorites([]);
-    }
-  }, []);
+	useEffect(() => {
+		var tmp = localStorage.getItem("favorites");
+		if (tmp) {
+			var fav = tmp.split(",");
+			setFavorites(fav);
+		} else {
+			setFavorites([]);
+		}
+	}, []);
 
-  const handleFormSubmit = (e) => {
-    if (e.key === 'Enter') {
-      var movie_name = e.target.value;
+	const handleFormSubmit = (e) => {
+		if (e.key === 'Enter') {
+			var movie_name = e.target.value;
 
-      var url = base_api_url + "?s=" + encodeURI(movie_name) + "&apikey=" + api_key;
+			var url = base_api_url + "?s=" + encodeURI(movie_name) + "&apikey=" + api_key;
 
 			fetch(url)
 				.then(response => response.json())
 				.then(json => setMovies(json.Search));
+        }
+	}
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+	}
+
+	const favoriteMovie = (e) => {
+		var id = ReactDOM.findDOMNode(e.target).parentNode.parentNode.id;
+		if (id) {
+			if (favorites.includes(id)) {
+				for (var i = 0; i < favorites.length; i++) {
+					if (favorites[i] === id) {
+						favorites.splice(i, 1);
+					}
+				}
+			} else {
+				favorites.push(id);
+			}
+		}
+
+		setFavorites(favorites);
+		localStorage.setItem("favorites", favorites);
     }
-  }
+    
+    const addCompareMovie = (e) => {
+        if(compareMovies.length < 2) {
+            var id = ReactDOM.findDOMNode(e.target).parentNode.id;
+            if (compareMovies.includes(id) === false) {
+                var index = -1;
+                for(var i = 0; i < movies.length; i++) {
+                    if(movies[i].imdbID === id) {
+                        index = i;
+                        break;
+                    }
+                }
+                compareMovies.push(movies[index]);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-  }
+                setCompareMovies(compareMovies);
 
-  const favoriteMovie = (e) => {
-    var id = ReactDOM.findDOMNode(e.target).parentNode.parentNode.id;
-		if(id) {
-      if(favorites.includes(id)) {
-        for( var i = 0; i < favorites.length; i++){ 
-          if ( favorites[i] === id) {
-            favorites.splice(i, 1); 
-          }
-       }
-      } else {
-        favorites.push(id);
-      }
+                window.scrollTo(0,document.body.scrollHeight);
+            }
+        }
     }
+    
+	return (
+		<div id="root">
+			<div className="favorites_menu">
+				<Link to={{
+					pathname: '/favorites',
+					state: favorites
+				}}>
+					<FontAwesomeIcon icon={faHeart} /><span>Favoritos</span>
+				</Link>
+			</div>
 
-    setFavorites(favorites);
-    localStorage.setItem("favorites", favorites);
-  }
+			<div className="header">
+				<Link className="link-tag" to="/"><h1>OMDd API</h1></Link>
+				<form id="search_movie" onSubmit={(e) => onSubmit(e)}>
+					<label>Nome do filme: </label>
+					<input name="movie_name" type="text" onKeyDown={(e) => handleFormSubmit(e)} />
+				</form>
+			</div>
 
-  return (
-    <div id="root">
-	  	<div className="favorites_menu">
-	  		<Link to={{
-          pathname: '/favorites',
-          state: favorites
-        }}>
-				  <FontAwesomeIcon icon={faHeart} /><span>Favoritos</span>		
-		  	</Link>
-	  	</div>
+			<div className="list_movies">
+				<ul className="list">
+					{movies.map(movie => {
+						return (
+							<li id={movie.imdbID} key={movie.imdbID} className="movie">
+								<FontAwesomeIcon onClick={(e) => favoriteMovie(e)} icon={faHeart} />
+                                <img onClick={(e) => addCompareMovie(e)} className="compare-movie" src="/compare-icon.png" />
+								<img className="poster" src={movie.Poster} />
+								<div className="movie_info">
+									<span id="title"><strong>Título: </strong>{movie.Title}</span>
+									<span id="year"><strong>Ano de lançamento: </strong>{movie.Year}</span>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 
-      <div className="header">
-        <Link className="link-tag" to="/"><h1>OMDd API</h1></Link>
-        <form id="search_movie" onSubmit={(e) => onSubmit(e)}>
-          <label>Nome do filme: </label>
-          <input name="movie_name" type="text" onKeyDown={(e) => handleFormSubmit(e)} />
-        </form>
-      </div>
-
-      <div className="list_movies">
-        <ul className="list">
-          {movies.map(movie => {
-            return (
-              <li id={movie.imdbID} key={movie.imdbID} className="movie">
-				<FontAwesomeIcon onClick={(e) => favoriteMovie(e)} icon={faHeart} />
-                <img src={movie.Poster} />
-                <div className="movie_info">
-                  <span id="title"><strong>Título: </strong>{movie.Title}</span>
-                  <span id="year"><strong>Ano de lançamento: </strong>{movie.Year}</span>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
+            <div className="list_movies">
+                <ul className="list">
+                    {compareMovies.map(cMovie => {
+						return (
+							<li id={cMovie.imdbID} key={cMovie.imdbID} className="movie">
+								<img className="poster" src={cMovie.Poster} />
+								<div className="movie_info">
+									<span id="title"><strong>Título: </strong>{cMovie.Title}</span>
+									<span id="year"><strong>Ano de lançamento: </strong>{cMovie.Year}</span>
+								</div>
+							</li>
+						);
+					})}
+                </ul>
+            </div>
+		</div>
+	);
 }
 
 export default App;
--- src/Favorites.js
+++ src/Favorites.js
@@ -1,9 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faThumbsDown, faThumbsUp, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
-import logo from './logo.svg';
 import './App.css';
 
 function Favorites() {
@@ -12,108 +11,108 @@ function Favorites() {
   const [movies, setMovies] = useState([]);
   
   useEffect(() => {
-    var fav = localStorage.getItem("favorites");
-    if(fav) {
-      var favorites = fav.split(",");
-      var response = [];
+	var fav = localStorage.getItem("favorites");
+	if(fav) {
+	  var favorites = fav.split(",");
+	  var response = [];
 
-      favorites.map(function(el, i) {
-        var url = base_api_url + "?i=" + el + "&apikey=" + api_key;
-        
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url, false);
-        xhr.onload = function (e) {
-          if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-              var movie_json = JSON.parse(xhr.responseText);
-              response.push({
-                "imdbID": movie_json.imdbID,
-                "Poster": movie_json.Poster,
-                "Title": movie_json.Title,
-                "Year": movie_json.Year
-              });
-            }
-          }
-        };
-        xhr.send(null);
-      });
+	  favorites.map(function(el, i) {
+		var url = base_api_url + "?i=" + el + "&apikey=" + api_key;
+		
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", url, false);
+		xhr.onload = function (e) {
+		  if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+			  var movie_json = JSON.parse(xhr.responseText);
+			  response.push({
+				"imdbID": movie_json.imdbID,
+				"Poster": movie_json.Poster,
+				"Title": movie_json.Title,
+				"Year": movie_json.Year
+			  });
+			}
+		  }
+		};
+		xhr.send(null);
+	  });
 
-      setMovies(response);
-    } else {
-      setMovies([]);
-    }
+	  setMovies(response);
+	} else {
+	  setMovies([]);
+	}
   }, []);
 
   const unfavoriteMovie = (e) => {
-    var id = ReactDOM.findDOMNode(e.target).parentNode.parentNode.id;
-    var favorites = localStorage.getItem("favorites");
-    if(favorites) {
-      favorites = favorites.split(",");
-      if(id) {
-        if(favorites.includes(id)) {
-          for( var i = 0; i < favorites.length; i++){ 
-            if ( favorites[i] === id) {
-              favorites.splice(i, 1); 
-            }
-         }
-        } else {
-          favorites.push(id);
-        }
-      }
-    }
+	var id = ReactDOM.findDOMNode(e.target).parentNode.parentNode.id;
+	var favorites = localStorage.getItem("favorites");
+	if(favorites) {
+	  favorites = favorites.split(",");
+	  if(id) {
+		if(favorites.includes(id)) {
+		  for( var i = 0; i < favorites.length; i++){ 
+			if ( favorites[i] === id) {
+			  favorites.splice(i, 1); 
+			}
+		 }
+		} else {
+		  favorites.push(id);
+		}
+	  }
+	}
 		
-    localStorage.setItem("favorites", favorites);
+	localStorage.setItem("favorites", favorites);
 
 
-    var response = [];
+	var response = [];
 
-    favorites.map(function(el, i) {
-      var url = base_api_url + "?i=" + el + "&apikey=" + api_key;
-      
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", url, false);
-      xhr.onload = function (e) {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            var movie_json = JSON.parse(xhr.responseText);
-            response.push({
-              "imdbID": movie_json.imdbID,
-              "Poster": movie_json.Poster,
-              "Title": movie_json.Title,
-              "Year": movie_json.Year
-            });
-          }
-        }
-      };
-      xhr.send(null);
-    });
+	favorites.map(function(el, i) {
+	  var url = base_api_url + "?i=" + el + "&apikey=" + api_key;
+	  
+	  var xhr = new XMLHttpRequest();
+	  xhr.open("GET", url, false);
+	  xhr.onload = function (e) {
+		if (xhr.readyState === 4) {
+		  if (xhr.status === 200) {
+			var movie_json = JSON.parse(xhr.responseText);
+			response.push({
+			  "imdbID": movie_json.imdbID,
+			  "Poster": movie_json.Poster,
+			  "Title": movie_json.Title,
+			  "Year": movie_json.Year
+			});
+		  }
+		}
+	  };
+	  xhr.send(null);
+	});
 
-    setMovies(response);
+	setMovies(response);
   }
 
   return (
-    <div id="root">
-      <div className="header">
-        <Link className="link-tag" to="/"><h1>OMDd API</h1></Link>
-      </div>
+	<div id="root">
+	  <div className="header">
+		<Link className="link-tag" to="/"><h1>OMDd API</h1></Link>
+	  </div>
 
-      <div className="list_movies">
-        <ul className="list">
-          {movies.map(movie => {
-            return (
-              <li id={movie.imdbID} key={movie.imdbID} className="movie">
-				        <FontAwesomeIcon onClick={(e) => unfavoriteMovie(e)} icon={faHeart} />
-                <img src={movie.Poster} />
-                <div className="movie_info">
-                  <span id="title"><strong>Título: </strong>{movie.Title}</span>
-                  <span id="year"><strong>Ano de lançamento: </strong>{movie.Year}</span>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
+	  <div className="list_movies">
+		<ul className="list">
+		  {movies.map(movie => {
+			return (
+			  <li id={movie.imdbID} key={movie.imdbID} className="movie">
+						<FontAwesomeIcon onClick={(e) => unfavoriteMovie(e)} icon={faHeart} />
+				<img src={movie.Poster} />
+				<div className="movie_info">
+				  <span id="title"><strong>Título: </strong>{movie.Title}</span>
+				  <span id="year"><strong>Ano de lançamento: </strong>{movie.Year}</span>
+				</div>
+			  </li>
+			);
+		  })}
+		</ul>
+	  </div>
+	</div>
   );
 }
 
